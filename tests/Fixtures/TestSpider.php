612121e0ab92a03ba4404b96819dddcc442e9628
Kai Sassnowski <me@kai-sassnowski.com>
2022-04-19 16:35:47 +0200

    Add test that default namespace is being registered correctly

---
+++ tests/Fixtures/TestSpider.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022 Kai Sassnowski
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/roach-php/laravel
+ */
+
+namespace RoachPHP\Laravel\Tests\Fixtures;
+
+use Generator;
+use RoachPHP\Http\Response;
+use RoachPHP\Spider\BasicSpider;
+
+/**
+ * @internal
+ */
+final class TestSpider extends BasicSpider
+{
+    public function parse(Response $response): Generator
+    {
+        yield from [];
+    }
+}
---
+++ tests/RegisterDefaultNamespaceTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022 Kai Sassnowski
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/roach-php/laravel
+ */
+
+namespace RoachPHP\Laravel\Tests;
+
+use Illuminate\Support\Facades\Artisan;
+use RoachPHP\Laravel\Tests\Fixtures\TestSpider;
+use RoachPHP\Roach;
+
+/**
+ * @internal
+ */
+final class RegisterDefaultNamespaceTest extends TestCase
+{
+    protected function tearDown(): void
+    {
+        Roach::restore();
+
+        parent::tearDown();
+    }
+
+    public function testUseDefaultNamespaceFromConfigFile(): void
+    {
+        config(['roach.default_spider_namespace' => 'RoachPHP\Laravel\Tests\Fixtures']);
+        $runner = Roach::fake();
+
+        Artisan::call('run', ['spider' => 'TestSpider']);
+
+        $runner->assertRunWasStarted(TestSpider::class);
+    }
+}
